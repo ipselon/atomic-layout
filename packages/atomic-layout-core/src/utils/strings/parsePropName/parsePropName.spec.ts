662e6ed80284c0e272ea967ac4e255a3cd86689d
Artem Zakharchenko <kettanaito@gmail.com>
2020-02-07 09:55:44 +0100

    @atomic-layout/core: Makes "parsePropName" case-sensitive

--- packages/atomic-layout-core/src/utils/strings/parsePropName/parsePropName.spec.ts
+++ packages/atomic-layout-core/src/utils/strings/parsePropName/parsePropName.spec.ts
@@ -2,77 +2,115 @@ import Layout from '../../../Layout'
 import parsePropName from './parsePropName'
 
 describe('parsePropName', () => {
-  it('parses a prop name without breakpoint or behavior', () => {
-    expect(parsePropName('gutter')).toEqual({
-      originPropName: 'gutter',
-      purePropName: 'gutter',
-      behavior: 'up',
-      breakpoint: {
-        name: 'xs',
-        isDefault: true,
-      },
+  describe('given layout with default breakpoints', () => {
+    it('parses a prop name without breakpoint or behavior', () => {
+      expect(parsePropName('gap')).toEqual({
+        originPropName: 'gap',
+        purePropName: 'gap',
+        behavior: 'up',
+        breakpoint: {
+          name: 'xs',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('parses a prop name with breakpoint without behavior', () => {
+      expect(parsePropName('gapMd')).toEqual({
+        originPropName: 'gapMd',
+        purePropName: 'gap',
+        behavior: 'up',
+        breakpoint: {
+          name: 'md',
+          isDefault: false,
+        },
+      })
+    })
+
+    it('parses prop name with behavior and without breakpoint', () => {
+      expect(parsePropName('gapDown')).toEqual({
+        originPropName: 'gapDown',
+        purePropName: 'gap',
+        behavior: 'down',
+        breakpoint: {
+          name: 'xs',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('parses a prop name with a breakpoint and behavior', () => {
+      expect(parsePropName('gapLgOnly')).toEqual({
+        originPropName: 'gapLgOnly',
+        purePropName: 'gap',
+        behavior: 'only',
+        breakpoint: {
+          name: 'lg',
+          isDefault: false,
+        },
+      })
+
+      expect(parsePropName('paddingVerticalMdDown')).toEqual({
+        originPropName: 'paddingVerticalMdDown',
+        purePropName: 'paddingVertical',
+        behavior: 'down',
+        breakpoint: {
+          name: 'md',
+          isDefault: false,
+        },
+      })
+    })
+
+    it('ignores matching breakpoint in wrong casing', () => {
+      expect(parsePropName('templatelg')).toEqual({
+        originPropName: 'templatelg',
+        purePropName: 'templatelg',
+        behavior: 'up',
+        breakpoint: {
+          name: 'xs',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('ignores matching behavior in wrong casing', () => {
+      expect(parsePropName('templateonly')).toEqual({
+        originPropName: 'templateonly',
+        purePropName: 'templateonly',
+        behavior: 'up',
+        breakpoint: {
+          name: 'xs',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('ignores matching breakpoint and behavior in wrong casing', () => {
+      expect(parsePropName('templatemdonly')).toEqual({
+        originPropName: 'templatemdonly',
+        purePropName: 'templatemdonly',
+        behavior: 'up',
+        breakpoint: {
+          name: 'xs',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('ignores unknown suffixes', () => {
+      expect(parsePropName('gapFoo')).toEqual({
+        originPropName: 'gapFoo',
+        purePropName: 'gapFoo',
+        behavior: 'up',
+        breakpoint: {
+          name: 'xs',
+          isDefault: true,
+        },
+      })
     })
   })
 
-  it('parses a prop name with breakpoint without behavior', () => {
-    expect(parsePropName('gutterMd')).toEqual({
-      originPropName: 'gutterMd',
-      purePropName: 'gutter',
-      behavior: 'up',
-      breakpoint: {
-        name: 'md',
-        isDefault: false,
-      },
-    })
-  })
-
-  it('parses prop name with behavior and without breakpoint', () => {
-    expect(parsePropName('gutterDown')).toEqual({
-      originPropName: 'gutterDown',
-      purePropName: 'gutter',
-      behavior: 'down',
-      breakpoint: {
-        name: 'xs',
-        isDefault: true,
-      },
-    })
-  })
-
-  it('parses a prop name with a breakpoint and behavior', () => {
-    expect(parsePropName('gutterLgOnly')).toEqual({
-      originPropName: 'gutterLgOnly',
-      purePropName: 'gutter',
-      behavior: 'only',
-      breakpoint: {
-        name: 'lg',
-        isDefault: false,
-      },
-    })
-
-    expect(parsePropName('paddingVerticalMdDown')).toEqual({
-      originPropName: 'paddingVerticalMdDown',
-      purePropName: 'paddingVertical',
-      behavior: 'down',
-      breakpoint: {
-        name: 'md',
-        isDefault: false,
-      },
-    })
-  })
-
-  it('ignores unknown suffixes', () => {
-    expect(parsePropName('gutterFoo')).toEqual({
-      originPropName: 'gutterFoo',
-      purePropName: 'gutterFoo',
-      behavior: 'up',
-      breakpoint: {
-        name: 'xs',
-        isDefault: true,
-      },
-    })
-  })
-
-  describe('with custom layout breakpoints', () => {
+  describe('given layout with custom breakpoints', () => {
     beforeAll(() => {
       Layout.configure({
         defaultBreakpointName: 'mobile',
@@ -142,10 +180,46 @@ describe('parsePropName', () => {
       })
     })
 
+    it('ignores matching breakpoint in wrong casing', () => {
+      expect(parsePropName('gaptablet')).toEqual({
+        originPropName: 'gaptablet',
+        purePropName: 'gaptablet',
+        behavior: 'up',
+        breakpoint: {
+          name: 'mobile',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('ignores matching behavior in wrong casing', () => {
+      expect(parsePropName('templateonly')).toEqual({
+        originPropName: 'templateonly',
+        purePropName: 'templateonly',
+        behavior: 'up',
+        breakpoint: {
+          name: 'mobile',
+          isDefault: true,
+        },
+      })
+    })
+
+    it('ignores matching breakpoint and behavior in wrong casing', () => {
+      expect(parsePropName('templatetabletonly')).toEqual({
+        originPropName: 'templatetabletonly',
+        purePropName: 'templatetabletonly',
+        behavior: 'up',
+        breakpoint: {
+          name: 'mobile',
+          isDefault: true,
+        },
+      })
+    })
+
     it('ignores unknown suffixes', () => {
-      expect(parsePropName('gutterFoo')).toEqual({
-        originPropName: 'gutterFoo',
-        purePropName: 'gutterFoo',
+      expect(parsePropName('gapFoo')).toEqual({
+        originPropName: 'gapFoo',
+        purePropName: 'gapFoo',
         behavior: 'up',
         breakpoint: {
           name: 'mobile',
